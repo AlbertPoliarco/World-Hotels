9d89b2618dc3f35ffff04917f538af9c4ff85c30
Nik Poliarco <nikpoliarco@Niks-MacBook-Pro.local>
2022-04-24 23:08:27 -0500

    places and pricing page added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import App from "./App"
+import About from "./components/About"
+import Places from "./components/Places"
+import Pricing from "./components/Pricing"
 
 ReactDOM.render(
   <Router>
     <Routes>
       <Route path='/' element={<App />} />
+      <Route path='/places' element={<Places />} />
+      <Route path='/pricing' element={<Pricing />} />
+      <Route path='/about' element={<About />} />
     </Routes>
   </Router>,
 
